ebf2dfdc3b09ee40ac71c72f78321eb7efc98253
Ivo van Dongen <info@ivovandongen.nl>
2022-07-07 17:41:59 +0300

    8.5

--- raytracing_one_weekend/main.cpp
+++ raytracing_one_weekend/main.cpp
@@ -8,10 +8,16 @@
 #include <iostream>
 #include <fstream>
 
-color ray_color(const ray &r, const hittable_list &world) {
+color ray_color(const ray &r, const hittable_list &world, uint depth) {
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth == 0) {
+        return {0, 0, 0};
+    }
+
     static hit_record hit{};
     if (world.hit(r, 0.001, infinity, hit)) {
-        return 0.5 * (hit.normal + color(1, 1, 1));
+        point3 target = hit.p + hit.normal + random_unit_vector();
+        return 0.5 * ray_color(ray(hit.p, target - hit.p), world, depth - 1);
     }
 
     // Background gradient
@@ -32,6 +38,7 @@ int main(int argc, char **argv) {
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     // Camera
     camera cam;
@@ -49,10 +56,10 @@ int main(int argc, char **argv) {
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0, 0, 0);
             for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
